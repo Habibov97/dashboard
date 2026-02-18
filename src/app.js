616fd4aafb038a34aef8f6b583cd0e9a791f60e9
Habibov97 <najaff.habibov@gmail.com>
2026-02-19 03:48:46 +0400

    Added globalErrorHandler Middleware to app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const config = require('./config');
 const express = require('express');
-const dbConnect = require('./config/database');
 const cors = require('cors');
+const AppError = require('./utils/error.utils');
+const dbConnect = require('./config/database');
 const router = require('./routes');
+const globalErrorHandler = require('./middlewares/globalErrorHandling.middleware');
 
 const app = express();
 
@@ -14,6 +16,12 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api', router);
+
+app.use((req, res, next) => {
+  next(new AppError('Cant find url on this server', 404));
+});
+
+app.use(globalErrorHandler);
 
 dbConnect().then(() => {
   app.listen(config.port, () => {
